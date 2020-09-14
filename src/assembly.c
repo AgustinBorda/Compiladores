0ce274a73dc1247b3c675739617b993af4c4d23a
TomasGuevara <renzotomasguevara@gmail.com>
2020-09-14 10:32:34 -0300

    solucion de problema lectura

--- src/assembly.c
+++ src/assembly.c
@@ -6,6 +6,10 @@ int open_file(tree head) {
 	FILE* f;
 
 	f = fopen("assembly.txt", "w+");
+	fprintf(f,"%s\n", "MOV EAX, 0");
+	fprintf(f,"%s\n", "MOV EBX, 0");
+	fprintf(f,"%s\n", "MOV ECX, 0");
+	fprintf(f,"%s\n", "MOV EDX, 0");
 	dfs(head, f);
 	fclose(f);
 	return 0;
@@ -13,7 +17,7 @@ int open_file(tree head) {
 
 
 void dfs(struct node *head,FILE* f) {
-   
+    
     if (head != NULL) {
         if (head -> hi != NULL) {
             dfs(head->hi, f);
@@ -28,7 +32,7 @@ void dfs(struct node *head,FILE* f) {
 
 void write(struct node * root, FILE* f) {
 	if (root ->flag == 0) {
-		fprintf(f,"%s%d\n" ,"MOV EAX, ", root -> data.var.value);
+		fprintf(f,"%s%d\n" ,"MOV EDX, ", root -> data.var.value);
 	}
 	if (root ->flag == 1) {
 		fprintf(f, "%s%d\n", "MOV ECX, ", root ->data.value);
@@ -42,28 +46,34 @@ void write(struct node * root, FILE* f) {
 				if(root -> hi -> flag == 1){
 					fprintf(f,"%s\n", "MOV EAX, 0");
 					fprintf(f,"%s%d%s\n" ,"ADD ", root -> hi -> data.value, ", EAX");
+				}else{
+					if((root -> hd -> data.op == '*') || (root -> hd -> data.op == '/')) {
+						fprintf(f,"%s\n" ,"ADD EBX, EAX");
+					}
 				}
 			}
 			if(root -> hd -> flag == 0){
 				fprintf(f,"%s%d%s\n" ,"ADD ", root -> hd -> data.var.value, ", EAX");
 			}else{
-				fprintf(f,"%s%d%s\n" ,"ADD ", root -> hd -> data.value, ", EAX");
+				if(root -> hd -> flag == 1){
+					fprintf(f,"%s%d%s\n" ,"ADD ", root -> hd -> data.value, ", EAX");
+				}
 			}
 		}
 		if(root -> data.op == '*') {
 			if(root -> hi -> flag == 0){
-				fprintf(f,"%s\n", "MOV EAX, 1");
-				fprintf(f,"%s%d%s\n" ,"MUL ", root -> hi -> data.var.value, ", EAX");
+				fprintf(f,"%s\n", "MOV EBX, 1");
+				fprintf(f,"%s%d%s\n" ,"MUL ", root -> hi -> data.var.value, ", EBX");
 			}else{
 				if(root -> hi -> flag == 1){
-					fprintf(f,"%s\n", "MOV EAX, 1");
-					fprintf(f,"%s%d%s\n" ,"MUL ", root -> hi -> data.value, ", EAX");
+					fprintf(f,"%s\n", "MOV EBX, 1");
+					fprintf(f,"%s%d%s\n" ,"MUL ", root -> hi -> data.value, ", EBX");
 				}
 			}
 			if(root -> hd -> flag == 0){
-				fprintf(f,"%s%d%s\n" ,"MUL ", root -> hd -> data.var.value, ", EAX");
+				fprintf(f,"%s%d%s\n" ,"MUL ", root -> hd -> data.var.value, ", EBX");
 			}else{
-				fprintf(f,"%s%d%s\n" ,"MUL ", root -> hd -> data.value, ", EAX");
+				fprintf(f,"%s%d%s\n" ,"MUL ", root -> hd -> data.value, ", EBX");
 			} 
 
 		}
@@ -75,6 +85,10 @@ void write(struct node * root, FILE* f) {
 				if(root -> hi -> flag == 1){
 					fprintf(f,"%s\n", "MOV EAX, 0");
 					fprintf(f,"%s%d%s\n" ,"DIF ", root -> hi -> data.value, ", EAX");
+				}else{
+					if((root -> hd -> data.op == '*') || (root -> hd -> data.op == '/')) {
+						fprintf(f,"%s\n" ,"ADD EBX, EAX");
+					}
 				}
 			}
 			if(root -> hd -> flag == 0){
